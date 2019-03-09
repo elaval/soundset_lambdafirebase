1c16a2c22e6a82dd5bbe85b405ede5de8cb24a27
Ernesto Laval <ernesto.laval@gmail.com>
2019-03-09 19:23:04 -0300

    Try with realtime firebase

--- index.js
+++ index.js
@@ -3,6 +3,27 @@ const admin = require('firebase-admin');
 //const FIREBASE_PRIVATE_KEY = JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
 const FIREBASE_PRIVATE_KEY = process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')
 
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: 'soundset-abffd',
+    clientEmail: 'firebase-adminsdk-zrq83@soundset-abffd.iam.gserviceaccount.com',
+    privateKey: FIREBASE_PRIVATE_KEY
+  }),
+  databaseURL: "https://soundset-abffd.firebaseio.com"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("test");
+/*
+ref.once("dummy", function(snapshot) {
+  console.log(snapshot.val());
+});
+*/
+
+
+/*
 admin.initializeApp({
   credential: admin.credential.cert({
     projectId: 'soundset-abffd',
@@ -12,7 +33,9 @@ admin.initializeApp({
 
 });
 
+
 const db = admin.firestore();
+*/
 
 exports.handler =  (event, context, callback) => {
   const data = {
@@ -22,7 +45,13 @@ exports.handler =  (event, context, callback) => {
   };
   
   // Add a new document in collection "cities" with ID 'LA'
-  const setDoc = db.collection('test').doc('LA').set(data);
+  
+  ref.once("value", function(snapshot) {
+    console.log(snapshot.val());
+  });
+  
+
+  //const setDoc = db.collection('test').doc('LA').set(data);
   /*
   var cityRef = db.collection('cities').doc('BJ');
 
@@ -34,7 +63,12 @@ exports.handler =  (event, context, callback) => {
   console.log("DONE");
   console.log(event);
   console.log(FIREBASE_PRIVATE_KEY);
-  callback(null, "DONE");
+
+  ref.once("value", function(snapshot) {
+    console.log(snapshot.val());
+    callback(null, snapshot.val());
+  });
+  
 };
 
 
